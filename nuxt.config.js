ca56f2cc2d37e736381d1e6b8bea58ebd66c3624
Sil van Diepen <me@silvandiepen.com>
2019-06-12 12:00:10 +0200

    will it work generated?

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ module.exports = {
 	/*
 	 ** Plugins to load before mounting the App
 	 */
-	plugins: [{ src: '~/plugins/vuex-persist.js' }],
+	plugins: [{ src: '~/plugins/vuex-persist.js', ssr: false }],
 
 	/*
 	 ** Nuxt.js modules
